aa401df42f63951c9b0aae1f01c749e07ab04af5
Jake <jakelovitz@gmail.com>
2019-07-17 13:26:56 -0400

    set up auth with login form

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,24 +1,24 @@
 class AuthController < ApplicationController
     def login
         user = User.find_by(username: params[:username])
-        byebug
+        # byebug
 
         if user && user.authenticate(params[:password])
 
-            token = encode_token(user.id)
+            # token = encode_token(user.id)
 
-            render json: {user: UserSerializer.new(user), token: token}
+            render json: {user: UserSerializer.new(user)}
         else
             render json: {errors: "you messed up goofball for shame"}
         end
     end
 
-    def auto_login
-        if session_user
-            render json: session_user
-        else
-            render json: {errors: "cookies y'all"}
-        end
-    end
+    # def auto_login
+    #     if session_user
+    #         render json: session_user
+    #     else
+    #         render json: {errors: "cookies y'all"}
+    #     end
+    # end
 
 end
