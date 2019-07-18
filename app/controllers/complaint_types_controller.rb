beaa33170aab1b7cb9557333056d8861ec20f42e
Jake <jakelovitz@gmail.com>
2019-07-18 18:00:30 -0400

    added form to create new ComplaintType

--- app/controllers/complaint_types_controller.rb
+++ app/controllers/complaint_types_controller.rb
@@ -6,4 +6,22 @@ class ComplaintTypesController < ApplicationController
         render json: @complaint_types
     end
 
+    def create
+        complaint_type = ComplaintType.new(
+            user_id: params["userId"],
+            name: params["complaintType"],
+            color: params["complaintColor"]
+        )
+        # byebug
+        complaint_type.save
+
+        render json: complaint_type
+    end
+
+    private
+
+    # def complaint_type_params
+    #     params.require(:complaint_type).permit(:user_id, :name, :color)
+    # end
+
 end
